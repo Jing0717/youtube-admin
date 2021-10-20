5546b5b8634b019f042782115100c5771c965976
Jing0717 <bleach81618@gmail.com>
2021-10-20 10:32:36 +0800

    create UserList & User pages

--- src/dummyData.js
+++ src/dummyData.js
@@ -49,3 +49,78 @@ export const userData = [
     },
 
 ];
+
+export const userRows = [
+    {
+        id: 1,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 2,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 3,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 4,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 5,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 6,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 7,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 8,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 9,
+        username: 'Jon Snow',
+        avatar: "https://i.pravatar.cc/300",
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+];
