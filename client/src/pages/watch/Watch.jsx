fa98759ba26f2fc49c758a79706d5e46114b4a02
Jing0717 <bleach81618@gmail.com>
2021-11-03 01:12:38 +0800

    Displaying Movies && Watch Page from API

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,19 +1,26 @@
 import { ArrowBackIosOutlined } from "@material-ui/icons";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import "./watch.scss";
 
 export default function Watch() {
+    const location = useLocation();
+    console.log(location)
+    const movie = location.movie;
     return (
         <div className="watch">
-            <div className="back">
-                <ArrowBackIosOutlined />
-                Home
-            </div>
+            <Link to="/">
+                <div className="back">
+                    <ArrowBackIosOutlined />
+                    Home
+                </div>
+            </Link>
             <video
                 className="video"
                 autoPlay
                 onProgress
                 controls
-                src="https://youtu.be/oumLyZkEFPw" />
+                src={movie.video} />
         </div>
     )
 }
