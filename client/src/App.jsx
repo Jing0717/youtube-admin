0eea458dde36061d6e5b8e26f467f1be1d9493e3
Jing0717 <bleach81618@gmail.com>
2021-11-03 01:12:38 +0800

    Displaying Movies && Watch Page from API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,45 @@
 import "./app.scss";
-import Login from "./pages/login/Login";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+// import { AuthContext } from "./authContext/AuthContext";
 
 const App = () => {
-  return <Login />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login"><Login /></Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movies" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
