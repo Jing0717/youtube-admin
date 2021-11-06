4eae420bb5c13339abf01727a1447b23543f978c
Jing0717 <bleach81618@gmail.com>
2021-11-06 15:10:11 +0800

    create Services

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Intro from '../components/Intro'
+import Services from '../components/Services'
+import { data } from '../data'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ services }) {
+  console.log(services)
   return (
     <div>
       <Head>
@@ -11,6 +14,14 @@ export default function Home() {
         <meta name="description" content="Best one" />
       </Head>
       <Intro />
+      <Services services={services} />
     </div>
   )
 }
+
+export const getStaticProps = () => {
+  const services = data;
+  return {
+    props: { services },
+  };
+};
